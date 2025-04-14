82814ae0f9799258e1609a807042841682fa57c0
Akhil Mantripragada <akhimantripragada@gmail.com>
2025-04-14 19:28:21 -0400

    Temporarily allow member access to GBP

--- app/Http/Controllers/AdsController.php
+++ app/Http/Controllers/AdsController.php
@@ -303,7 +303,7 @@ class AdsController extends Controller
 
                 // Check if the current user is a PRIMARY_OWNER or OWNER
                 foreach ($admins as $admin) {
-                    if (isset($admin['account']) && $admin['account'] === $accountId && in_array($admin['role'], ['PRIMARY_OWNER', 'OWNER'])) {
+                    if (isset($admin['account']) && $admin['account'] === $accountId && in_array($admin['role'], ['PRIMARY_OWNER', 'OWNER', 'MANAGER'])) {
                         $filteredLocations[] = $location;
                         break; // No need to check further admins for this location
                     }
@@ -424,7 +424,7 @@ class AdsController extends Controller
 
     }
 
-    
+
     /*
     * Create a Google Ads Account
     */
